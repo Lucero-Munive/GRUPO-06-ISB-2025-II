6cedeb9d53608372f9c7758832ddb893f6a35681
Alvaro Untiveros <alvarountiveros2005@gmail.com>
2025-12-07 05:29:09 -0500

    a

--- Proyecto/Software/frontend/src/lib/firebase.ts
+++ Proyecto/Software/frontend/src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // Tu configuración real del proyecto 'studio-...' que vimos en la consola
 const firebaseConfig = {
-  apiKey: "AIzaSyDChlcc7jLU0E4j-b3d7jCbFl08tAD4ncI",
-  authDomain: "studio-6590148871-6778d.firebaseapp.com",
-  projectId: "studio-6590148871-6778d",
-  storageBucket: "studio-6590148871-6778d.firebasestorage.app",
-  messagingSenderId: "65187920779",
-  appId: "1:65187920779:web:01f78994224d80f802cd8e"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Inicialización Singleton (Evita que Next.js se queje si recargas la página)
